8dd2e74636a58c725cf50e2549940e94f4147c5d
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-11-15 23:11:39 +0200

    fix hero styles and add target in footer socisl links

--- src/components/SocialLinks/SocialLinksFooter.jsx
+++ src/components/SocialLinks/SocialLinksFooter.jsx
@@ -8,14 +8,23 @@ const SocialLinksFooter = ({ className }) => {
       {socialLinks.map((item) => {
         return (
           <li key={item.id} className={styles.socialItem}>
-            <a href={item.href} target="_blank" className={styles.socialIcon}>
+            <a
+              href={item.href}
+              {...(item.title !== 'Telephone' && {
+                target: '_blank',
+                rel: 'noopener noreferrer',
+              })}
+              className={styles.socialIcon}
+            >
               <div className={styles.icon}>
                 <svg>
                   <use href={item.icon} />
                 </svg>
               </div>
-              
-              <p className={styles.subtitle + ' ' + 'textLinkAnimation'}>{item.subtitle}</p>
+
+              <p className={styles.subtitle + ' ' + 'textLinkAnimation'}>
+                {item.subtitle}
+              </p>
             </a>
           </li>
         );
