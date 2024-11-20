ea90b2bde24fe9f6cf2be8e6c8b6e042ca3df00b
Andrey Tsapkov <tsapkov1994@gmail.com>
2024-11-20 23:49:23 +0200

    fix styles in apartments and filter

--- src/components/BreadCrumbs/BreadCrumbs.jsx
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -1,20 +1,14 @@
-import styles from './BreadCrumbs.module.scss';
+import styles from "./BreadCrumbs.module.scss";
 
-
-const BreadCrumbs = ({ onClick, title }) => {
-
-    return (
-        <div
-            className={styles.wrapper}
-            onClick={onClick}
-        >
-            <svg className={styles.backIcon}>
-                <use href="../sprite.svg#icon-back" />
-            </svg>
-            <p>{title}</p>
-        </div>
-    );
+const BreadCrumbs = ({ onClick, title, externalClass }) => {
+  return (
+    <div className={`${styles.wrapper} ${externalClass}`} onClick={onClick}>
+      <svg className={styles.backIcon}>
+        <use href="../sprite.svg#icon-back" />
+      </svg>
+      <p>{title}</p>
+    </div>
+  );
 };
-
 
 export default BreadCrumbs;
