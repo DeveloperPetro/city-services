40e992c6883b7b8e1b908c09a5410c1142ba0c5b
Denis <denis.tolmachev@gmail.com>
2024-11-24 19:03:59 +0200

    phone-buttons-fix

--- src/components/CallBtn/CallBtn.jsx
+++ src/components/CallBtn/CallBtn.jsx
@@ -1,24 +1,24 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { socialLinks } from "@/data";
 import styles from "./CallBtn.module.scss";
-
 
 const CallBtn = ({ className }) => {
   const { t } = useTranslation();
   const [isLoading, setIsLoading] = useState(true);
 
+  // Найти объект с телефоном
+  const phoneLink = socialLinks.find(link => link.title === "Telephone")?.href;
+
   useEffect(() => {
     setIsLoading(false);
   }, []);
+
   return (
-    <>
-      <a href={socialLinks.href} className={styles.button + " " + `${className}`}>
-        {!isLoading && t("Buttons.CalltBtn")}
-      </a>
-    </>
+    <a href={phoneLink} className={`${styles.button} ${className}`}>
+      {!isLoading && t("Buttons.CalltBtn")}
+    </a>
   );
 };
 
