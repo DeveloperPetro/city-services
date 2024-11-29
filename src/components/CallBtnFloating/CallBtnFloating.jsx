bf203a1e1289a10dc17f94d440c149f02e4da220
Maksimchik36 <Peltek1985@gmail.com>
2024-11-29 11:32:25 +0200

    created condition for removing btn from dashboard

--- src/components/CallBtnFloating/CallBtnFloating.jsx
+++ src/components/CallBtnFloating/CallBtnFloating.jsx
@@ -1,19 +1,30 @@
+"use client";
+import { usePathname } from 'next/navigation';
 import { socialLinks } from '@/data';
 import styles from './CallBtnFloating.module.scss';
 
+
 const CallBtnFloating = () => {
-  const phoneLink = socialLinks.find(
-    (link) => link.title === 'Telephone'
+  const phoneLink = socialLinks.find(link => link.title === 'Telephone'
   )?.href;
+
+  const pathname = usePathname();
+  const isDashboard = pathname.startsWith("/dashboard");
+
+
   return (
-    <div className={styles.callBtn}>
-      <a href={phoneLink}>
-        <svg>
-          <use href="/sprite.svg#phone-new" />
-        </svg>
-      </a>
-    </div>
+    <>
+      {!isDashboard && <div className={styles.callBtn}>
+        <a href={phoneLink}>
+          <svg>
+            <use href="/sprite.svg#phone-new" />
+          </svg>
+        </a>
+      </div>
+      }
+    </>
   );
 };
+
 
 export default CallBtnFloating;
