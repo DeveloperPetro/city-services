75b7d38ecbb059606ef168b9e6be932e9161932f
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2024-12-01 17:36:02 +0200

    change padding in header and edits CallBtnFloating isBottom

--- src/components/CallBtnFloating/CallBtnFloating.jsx
+++ src/components/CallBtnFloating/CallBtnFloating.jsx
@@ -1,12 +1,32 @@
+'use client';
+
 import { socialLinks } from '@/data';
 import styles from './CallBtnFloating.module.scss';
+import { useEffect, useState } from 'react';
 
 const CallBtnFloating = () => {
+  const [hidden, setHidden] = useState(false);
+
   const phoneLink = socialLinks.find(
     (link) => link.title === 'Telephone'
   )?.href;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBottom =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight;
+      setHidden(isBottom);
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.callBtn}>
+    <div className={`${styles.callBtn} ${hidden ? styles.hidden : ''}`}>
       <a href={phoneLink}>
         <svg>
           <use href="/sprite.svg#phone-new" />
--- src/components/CallBtnRound/CallBtnRound.jsx
+++ src/components/CallBtnRound/CallBtnRound.jsx
@@ -1,12 +1,14 @@
 'use client';
 
 import React from 'react';
-import { socialLinks } from "@/data";
+import { socialLinks } from '@/data';
 import styles from './CallBtnRound.module.scss';
 
 const CallBtnRound = ({ text }) => {
   // Найти объект с телефоном
-  const phoneLink = socialLinks.find(link => link.title === 'Telephone')?.href;
+  const phoneLink = socialLinks.find(
+    (link) => link.title === 'Telephone'
+  )?.href;
 
   return (
     <div className={styles.callBtn}>
